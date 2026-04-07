f893fe4fa3f1eb365845922b6359e8720f904f71
Josias Ataide <jataides.ja@gmail.com>
2026-04-07 15:51:30 -0300

    chore: update dependencies to latest and configure next.config.mjs for build compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   typescript: {
     // ⚠️ Ignora erros de tipagem no build para garantir o deploy
     ignoreBuildErrors: true,
@@ -7,6 +8,9 @@ const nextConfig = {
   eslint: {
     // ⚠️ Ignora avisos de linter no build
     ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true
   }
 };
 
